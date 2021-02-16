9423bee37802b7a8e1c47919cae63bad6d2dff3f
Khairul Taher <khairultaher@outlook.com>
2021-02-16 11:49:22 +0600

    1.3

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const connectDatabase = require('./config/database')
 
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const cloudinary = require('cloudinary')
 
 // Handle Uncaught exceptions
@@ -12,10 +12,11 @@ process.on('uncaughtException', err => {
 })
 
 // Setting up config file
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
-
-//dotenv.config({ path: 'backend/config/config.env' })
-
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config({ path: 'backend/config/config.env' })
+} else {
+    dotenv.config({ path: 'backend/config/config.env' })
+}
 
 // Connecting to database
 connectDatabase();
